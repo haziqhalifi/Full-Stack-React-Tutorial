cc0dbd6e6fe28f1dbf77757aedaa721c42f2b0e1
haziqhalifi <haziq.halifi@gmail.com>
2024-12-15 15:15:57 +0800

    Route Parameters in Express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -4,14 +4,18 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/hello', function(req, res) {
+app.get('/hello', function (req, res) {
     res.send('Hello!');
 });
 
-app.post('/hello', function(req, res ) {
-    res.send('Hello '+req.body.name+' from Post Method')
+app.get('/hello/:name', function (req, res) {
+    res.send('Hello ' + req.params.name)
+})
+
+app.post('/hello', function (req, res) {
+    res.send('Hello ' + req.body.name + ' from Post Endpoint')
 });
 
-app.listen(8000, function() {
+app.listen(8000, function () {
     console.log("Server is listening on port 8000");
 });
