377d1c9d4189b24f45887b885b28f0d43fc86db1
haziqhalifi <haziq.halifi@gmail.com>
2024-12-15 23:02:43 +0800

    Upvoting Articles

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,19 +1,36 @@
 import express from "express";
 
+const articleInfo = [
+    { name: 'learn-node', upvotes: 0 },
+    { name: 'learn-react', upvotes: 0 },
+    { name: 'mongodb', upvotes: 0 },
+]
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', function (req, res) {
-    res.send('Hello!');
-});
+// app.get('/hello', function (req, res) {
+//     res.send('Hello!');
+// });
 
-app.get('/hello/:name', function (req, res) {
-    res.send('Hello ' + req.params.name)
-})
+// app.get('/hello/:name', function (req, res) {
+//     res.send('Hello ' + req.params.name)
+// })
 
-app.post('/hello', function (req, res) {
-    res.send('Hello ' + req.body.name + ' from Post Endpoint')
+// app.post('/hello', function (req, res) {
+//     res.send('Hello ' + req.body.name + ' from Post Endpoint')
+// });
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    console.log(`Received request to upvote article: ${req.params.name}`);
+    const article = articleInfo.find(a => a.name === req.params.name);
+    if (!article) {
+        return res.status(404).send('Article not found');
+    }
+    article.upvotes += 1;
+
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!')
 });
 
 app.listen(8000, function () {
