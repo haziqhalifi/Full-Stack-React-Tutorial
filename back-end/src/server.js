12e5032f7aa9f3c9bc3d1d65393a74905af44e50
haziqhalifi <haziq.halifi@gmail.com>
2024-12-20 01:19:16 +0800

    Add mongodb to express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
 const articleInfo = [
     { name: 'learn-node', upvotes: 0, comments: [] },
@@ -21,6 +22,37 @@ app.use(express.json());
 // app.post('/hello', function (req, res) {
 //     res.send('Hello ' + req.body.name + ' from Post Endpoint')
 // });
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const uri = 'mongodb://127.0.0.1:27017';
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+
+    try {
+        await client.connect();
+        const db = client.db('full-stack-react-db');
+        const article = await db.collection('articles').findOne({ name });
+
+        if (!article) {
+            return res.status(404).send('Article not found');
+        }
+
+        res.json(article);
+    } catch (error) {
+        console.error('Error fetching article:', error);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        await client.close();
+    }
+});
 
 app.post('/api/articles/:name/upvote', (req, res) => {
     console.log(`Received request to upvote article: ${req.params.name}`);
