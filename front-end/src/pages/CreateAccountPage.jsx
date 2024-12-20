73b462b4503989a3503737e0ec3e2063f6543b5f
haziqhalifi <haziq.halifi@gmail.com>
2024-12-20 15:34:37 +0800

    Build a create account page

--- front-end/src/pages/CreateAccountPage.jsx
+++ front-end/src/pages/CreateAccountPage.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 export default function CreateAccountPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    async function createAccount() {
+        if (password !== confirmPassword) {
+            setError('Password and Confirm Password do not match!')
+            return;
+        }
+        try {
+            await createUserWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message);
+        }
+    }
+
     return (
-        <div>
-            <h1>Create Account</h1>
-        </div>
+        <>
+            <div className="flex flex-col items-center h-screen">
+                <h1 className="text-3xl font-bold text-slate-800 dark:text-white">Create Account</h1>
+                {error && <p className="text-red-500">{error}</p>}
+                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Your Email Address" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Confirm Password" type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
+                <button className="mt-2 text-slate-600 hover:text-slate-800 bg-slate-200 rounded-md px-2 py-1 hover:bg-slate-300 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700" onClick={createAccount}>Create Account</button>
+                <Link className="text-slate-600 hover:text-slate-800 rounded-md px-2 py-1 dark:text-slate-300 hover:underline" to='/login'>Already have an account? Login here.</Link>
+            </div>
+        </>
     );
 }
--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -23,9 +23,9 @@ export default function LoginPage() {
         <>
             <div className="flex flex-col items-center h-screen">
                 <h1 className="text-3xl font-bold text-slate-800 dark:text-white">Login</h1>
-                {error && <p>{error}</p>}
-                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Your Email Address" value={email} onChange={e => setEmail(e.target.value)} />
-                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                {error && <p className="text-red-500">{error}</p>}
+                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Your Email Address" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                <input className="mt-2 block bg-white dark:bg-slate-700 text-slate-800 dark:text-slate-300 border border-slate-300 dark:border-slate-600 rounded-md" placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} />
                 <button className="mt-2 text-slate-600 hover:text-slate-800 bg-slate-200 rounded-md px-2 py-1 hover:bg-slate-300 dark:bg-slate-800 dark:text-slate-300 dark:hover:bg-slate-700" onClick={logIn}>Log In</button>
                 <Link className="text-slate-600 hover:text-slate-800 rounded-md px-2 py-1 dark:text-slate-300 hover:underline" to='/create-account'>Don't have an account? Create one here.</Link>
             </div>
